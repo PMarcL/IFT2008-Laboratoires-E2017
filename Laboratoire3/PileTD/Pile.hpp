8d609dc3e291fb45691061f5829196ad8b96eaad
Pierre-Marc Levasseur <pmleva@hotmail.com>
2017-05-17 12:30:54 -0400

    Préparation labo3

---
+++ Laboratoire3/PileTD/Pile.hpp
@@ -0,0 +1,23 @@
+#include "ContratException.h"
+
+namespace labPileFile {
+
+// À compléter ...
+
+template<typename T>
+std::ostream & operator <<(std::ostream & p_out, const Pile<T> & p_source) {
+	typename Pile<T>::Noeud * tmp = p_source.m_sommet;
+	p_out << "Pile: [";
+	while (tmp != NULL) {
+		p_out << tmp->m_el;
+		if (tmp->m_suivant != NULL)
+			p_out << ",";
+		tmp = tmp->m_suivant;
+	}
+	p_out << "]";
+	return p_out;
+}
+
+// Méthodes privées
+
+}//Fin du namespace
