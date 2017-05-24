03fc3ba94b6912da4722f874b6162c14cfc9e798
Pierre-Marc Levasseur <pmleva@hotmail.com>
2017-05-24 17:53:12 -0400

    setup graphe matrice

---
+++ Laboratoire4/GrapheMatrice/Graphe.hpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include "ContratException.h"
+
+using namespace std;
+
+namespace labGrapheListes {
+
+// implémentation ici
+
+template<typename T>
+void Graphe<T>::affiche() const {
+
+	for (unsigned int i = 0; i < m_nbSommets; ++i) {
+		std::cout << "Sommet " << i << ": ";
+
+		for (unsigned int j = 0; j < m_nbSommets; ++j) {
+			if (m_matriceAdj[i][j]) {
+				std::cout << i << "->" << j << ", ";
+			}
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+
+}
+
+} //Fin du namespace
