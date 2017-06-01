ad6a7873c438f8d49a3ed021af263e4e7df88623
Pierre-Marc Levasseur <pmleva@hotmail.com>
2017-06-01 10:12:41 -0400

    solution labo 5

--- Laboratoire4/GrapheListe/Graphe.hpp
+++ Laboratoire4/GrapheListe/Graphe.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <stack>
+#include <queue>
 #include "ContratException.h"
 
 namespace labGrapheListes {
@@ -98,6 +99,106 @@ template<typename T>
 unsigned int Graphe<T>::degreSortieSommet(unsigned int sommet) const {
 	PRECONDITION(sommet < m_nbSommets);
 	return m_listesAdj[sommet].size();
+}
+
+template<typename T>
+std::vector<unsigned int> Graphe<T>::parcoursProfondeur(unsigned int sommet) const {
+	std::stack<unsigned int> pile;
+	std::vector<bool> sommetsVisites(m_nbSommets, false);
+	std::vector<unsigned int> resultat;
+
+	sommetsVisites[sommet] = true;
+	pile.push(sommet);
+
+	while (!pile.empty()) {
+		auto prochain = pile.top();
+		pile.pop();
+		resultat.push_back(prochain);
+
+		auto voisins = listerSommetsAdjacents(prochain);
+		for (auto voisin : voisins) {
+			if (!sommetsVisites[voisin]) {
+				pile.push(voisin);
+				sommetsVisites[voisin] = true;
+			}
+		}
+	}
+	return resultat;
+}
+
+template<typename T>
+std::vector<unsigned int> Graphe<T>::parcoursLargeur(unsigned int sommet) const {
+	std::queue<unsigned int> file;
+	std::vector<bool> sommetsVisites(m_nbSommets, false);
+	std::vector<unsigned int> resultat;
+
+	sommetsVisites[sommet] = true;
+	file.push(sommet);
+
+	while (!file.empty()) {
+		auto prochain = file.front();
+		file.pop();
+		resultat.push_back(prochain);
+
+		auto voisins = listerSommetsAdjacents(prochain);
+		for (auto voisin : voisins) {
+			if (!sommetsVisites[voisin]) {
+				file.push(voisin);
+				sommetsVisites[voisin] = true;
+			}
+		}
+	}
+	return resultat;
+}
+
+template<typename T>
+bool Graphe<T>::estConnexe() const {
+	Graphe < T > copie(*this);
+
+	for (unsigned int i = 0; i < m_nbSommets; i++) {
+		for (unsigned int j = 0; j < m_nbSommets; j++) {
+			if (copie.arcExiste(i, j) && !copie.arcExiste(j, i)) {
+				copie.ajouterArc(j, i);
+			}
+		}
+	}
+
+	auto sommetsConnexes = copie.parcoursLargeur(0);
+	return sommetsConnexes.size() == m_nbSommets;
+}
+
+template<typename T>
+std::vector<unsigned int> Graphe<T>::triTopologique() const {
+	std::list<unsigned int> triTopo;
+	std::vector < EtatSommet > sommetsVisites(m_nbSommets, EtatSommet::PasVisite);
+
+	for (unsigned int i = 0; i < m_nbSommets; i++) {
+		if (sommetsVisites[i] == EtatSommet::PasVisite) {
+			visiterSommet(i, sommetsVisites, triTopo);
+		}
+	}
+
+	return std::vector<unsigned int>(triTopo.begin(), triTopo.end());
+}
+
+template<typename T>
+void Graphe<T>::visiterSommet(unsigned int sommet, std::vector<EtatSommet> &sommetsVisites,
+		std::list<unsigned int> &triTopo) const {
+	if (sommetsVisites[sommet] == EtatSommet::EnCoursDeVisite) {
+		// le graphe a un cycle
+		PRECONDITION(false);
+	}
+	if (sommetsVisites[sommet] == EtatSommet::PasVisite) {
+		sommetsVisites[sommet] = EtatSommet::EnCoursDeVisite;
+
+		auto voisins = listerSommetsAdjacents(sommet);
+		for (auto voisin : voisins) {
+			visiterSommet(voisin, sommetsVisites, triTopo);
+		}
+
+		sommetsVisites[sommet] = EtatSommet::Visite;
+		triTopo.push_front(sommet);
+	}
 }
 
 } //Fin du namespace
